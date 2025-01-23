14a5228119e0b47c3e281f9ab5a68dc9107c60f1
Justine Apdal <apdaljustine@gmail.com>
2025-01-23 19:29:26 +0800

    Connected firebase to web?

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:pinesville_admin/dashboard.dart';
 import 'package:pinesville_admin/login.dart';
 import 'package:pinesville_admin/chats.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
   runApp(MyApp());
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+      // .then((_) => Get.put(AuthenticationRepository()));
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
